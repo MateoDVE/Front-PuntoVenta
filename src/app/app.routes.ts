329b9e19927472ed34e46786b8cacab629f90cb3
MateoDVE <matu2207@gmail.com>
2026-03-29 18:29:21 -0400

    feat: implement role-based routing and user session management

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,16 +4,34 @@ import { DashboboardAdmin } from './Pages/admin/dashboboard-admin/dashboboard-ad
 import { GestionVendedoresComponent } from './Pages/admin/gestion-vendedores/gestion-vendedores';
 import { authGuard } from './guards/auth.guard';
 import { Catalogo } from './Pages/admin/catalogo/catalogo';
+import { DashboardVendedor } from './Pages/vendedor/dashboard-vendedor/dashboard-vendedor';
 
 export const routes: Routes = [
 
   { path: 'login', component: Iniciarsesion },
 
-  { path: 'admin/dashboard', component: DashboboardAdmin, canActivate: [authGuard] },
+  {
+    path: 'admin/dashboard',
+    component: DashboboardAdmin,
+    canActivate: [authGuard],
+    data: { roles: ['admin', 'administrador'] }
+  },
 
-  { path: 'admin/gestion-vendedores', component: GestionVendedoresComponent, canActivate: [authGuard] },
+  {
+    path: 'admin/gestion-vendedores',
+    component: GestionVendedoresComponent,
+    canActivate: [authGuard],
+    data: { roles: ['admin', 'administrador'] }
+  },
 
-  { path: 'admin/catalogo', component: Catalogo, canActivate: [authGuard] },
+  {
+    path: 'admin/catalogo',
+    component: Catalogo,
+    canActivate: [authGuard],
+    data: { roles: ['admin', 'administrador'] }
+  },
+
+  { path: 'vendedor/dashboard', component: DashboardVendedor, canActivate: [authGuard],data: { roles: ['vendedor'] }},
 
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 
