7bb893f147881e3689f90e62cebd003fdf64733a
hassan <davidlopezolivares05@gmail.com>
2026-04-15 20:26:21 -0400

    correcion back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://back-puntoventa-1.onrender.com',
+  apiUrl: 'https://back-puntoventa-2.onrender.com',
   googleMapsKey: 'AIzaSyAbbumO3yYDHqn_TKs-onKq2sFz5uubKYc'
 };
