4aac71a77ed8e2098428be1dae1ddc971950a4c1
aleloayza <alejandraloayza68@gmail.com>
2026-04-23 15:38:36 -0400

    crear componente venta-vendedor

---
+++ src/app/Pages/vendedor/venta-vendedor/venta-vendedor.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-venta-vendedor',
+  imports: [],
+  templateUrl: './venta-vendedor.html',
+  styleUrl: './venta-vendedor.scss',
+})
+export class VentaVendedor {
+
+}
